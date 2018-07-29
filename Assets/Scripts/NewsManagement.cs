4b3f763232430d18e2257d8a147b67bc416fadc8
ChristianAeberhard <christian.aeberhard2@uzh.ch>
2018-07-29 17:50:13 +0200

    NewsManagementUpdate
    Now the script behaves dynamicly, such that when multiple messages are spawned at the same timeframe, they are added to the stack and are displayed one after the other according to their order.

--- Assets/Scripts/NewsManagement.cs
+++ Assets/Scripts/NewsManagement.cs
@@ -3,65 +3,109 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewsManagement : MonoBehaviour {
-    public GameObject NewsPanel;//Used for instantiating the NewsPanel Sprite
+
+//This skript is for dynamicly adding NewsMessages to the game. The order of display in case of News Overlap is handled through a LastInFirstOut Stack.
+//To add new messages, don't forget to adjust the dictonary {time Stamp,"News Message} either aswell as the StackSetupTimeEvents. To add news from outside,
+//like news that are dependent on Player action and not on Time, simply add to the stack by using newsStack.Add(x);, whereas x is the timestamp in the dict.
+//For repeating messages such as "The upgrade for your building is complete" we can use specific timestamps in the dict such as
+//{9999,"The upgrade..."}.
+public class NewsManagement : MonoBehaviour
+{
+    public GameObject newsPanel;//Used for instantiating the NewsPanel Sprite
     public Text newsText;//Used for adjusting the Message inside the Panel
     private bool newsCast = true; //used to avoid the animation playing more than once
-    private GameObject NewsBox;
+    private GameObject newsBox;
+    public List<int> newsStack = new List<int>();//Stack Datastructure used for queueing News if News overlap LiFo (LastIn/FirstOut), we save the dictText Keys in the Stack
+    private bool stopper = false;
+    private int stackLength;
 
-    Dictionary<int,string> dictTexts = new Dictionary<int, string>()//Dict with all the News Messages
+    Dictionary<int, string> dictTexts = new Dictionary<int, string>()//Dict with all the News Messages
     {
-        {5,"News1: Ein Fremder trifft in eurem Lager ein und bittet euch um einen gefallen..."},
-        {20,"News2"},
-        {40,"News3"},
-        {60,"News4"}
+        {2,"Ein Fremder trifft in eurem Lager ein und bittet euch um einen gefallen..."},
+        {8,"Get Rekt or Git Better"},
+        {20,"Hoi zamen, ih verchaufe eu eh Bier für 15 SCHÄDE!"},
+        {30,"News3"},
+        {40,"News4"}
     };
 
-    public bool SetNewsText(float seconds)//This Function feeds the Dictionary into the animated Panel
+    private int getStackLength()
     {
-        int secondsRounded = (int)Mathf.Round(seconds);//Rounds the seconds float to a whole integer
-
-        //following are all the timebased, scripted News at the secondsRounded Mark
-        if(secondsRounded == 5)
-        {
-            newsText.GetComponentInChildren<Text>().text = dictTexts[secondsRounded];
-            return true;
-
-        } else if (secondsRounded == 20)
-        {
-            newsText.GetComponentInChildren<Text>().text = dictTexts[secondsRounded];
-            return true;
-
-        }else if (secondsRounded == 40)
-        {
-            newsText.GetComponentInChildren<Text>().text = dictTexts[secondsRounded];
-            return true;
-
-        }else if (secondsRounded == 60)
-        {
-            newsText.GetComponentInChildren<Text>().text = dictTexts[secondsRounded];
-            return true;
-        }
-        return false;
+        stackLength = newsStack.Count;
+        return stackLength-1;//-1 because Lists start at index 0, not 1
     }
-	
+
     public void DisplayNews(float seconds)//seconds is the global time in which we know when to trigger which news
     {
-        if (SetNewsText(seconds) && newsCast)
+
+        StackSetupTimeEvents(seconds);
+        if (getStackLength() >= 0 && newsCast)
         {
-            NewsBox = Instantiate(NewsPanel, transform.position = new Vector3(35f, 152f, 0), Quaternion.identity) as GameObject;
-            NewsBox.transform.SetParent(GameObject.Find("MiddleCanvas").transform, false);//mittels false skaliert das instantiierte Objekt an den Globalen X und Y, nicht am Parent
-            NewsBox.transform.SetParent(GameObject.Find("NewsPanel").transform, false);
-            float delay = GameObject.Find("NewsPanel").GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).length;//length of the Animator Clip of the Panel
-            StartCoroutine (WaitTime(delay));//with this syntax we call the IEnumerator WaitTime
-            Destroy(NewsBox, delay);//Delets Object afer "delay" seconds
-            newsCast = false;//bool to false so the animation is not starting again
-        }   
+            for (int i = getStackLength(); i >= 0; i--)
+            {
+                newsText.GetComponentInChildren<Text>().text = dictTexts[newsStack[i]];//get the NewsText from the Dictonary
+                if (newsCast)
+                {
+                    newsStack.RemoveAt(getStackLength());
+                    newsBox = Instantiate(newsPanel, transform.position = new Vector3(35f, 152f, 0), Quaternion.identity) as GameObject;
+                    newsBox.transform.SetParent(GameObject.Find("MiddleCanvas").transform, false);//mittels false skaliert das instantiierte Objekt an den Globalen X und Y, nicht am Parent
+                    newsBox.transform.SetParent(GameObject.Find("NewsPanel").transform, false);
+                    float delay = GameObject.Find("NewsPanel").GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).length;//length of the Animator Clip of the Panel
+                    StartCoroutine(WaitTimePanelAnimation(delay));//with this syntax we call the IEnumerator WaitTime
+                    Destroy(newsBox, delay);//Delets Object afer "delay" seconds
+                    newsCast = false;//bool to false so the animation is not starting again
+                }
+            }
+        }
+
+        
     }
 
-    IEnumerator WaitTime(float seconds)//This Function is used to wait "float seconds" seconds until the code below yield return... is executed
+    IEnumerator WaitTimePanelAnimation(float seconds)//This Function is used to wait "float seconds" seconds until the code below yield return... is executed
     {
         yield return new WaitForSeconds(seconds);
         newsCast = true;
+    }
+
+    IEnumerator WaitStackAdd(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        stopper = false;
+    }
+
+    private void StackSetupTimeEvents(float seconds)
+    {
+        int secondsRounded = (int)Mathf.Round(seconds);//Rounds the seconds float to a whole integer
+        if (!stopper)
+        {
+            //following are all the timebased, scripted News at the "secondsRounded" Mark
+            if (secondsRounded == 2)
+            {
+                newsStack.Add(secondsRounded);
+                newsStack.Add(20);//just for testing
+                stopAddingToStack();
+            }
+            else if (secondsRounded == 8)
+            {
+                newsStack.Add(secondsRounded);
+                stopAddingToStack();
+            }
+            else if (secondsRounded == 30)
+            {
+                newsStack.Add(secondsRounded);
+                stopAddingToStack();
+            }
+            else if (secondsRounded == 40)
+            {
+                newsStack.Add(secondsRounded);
+                stopAddingToStack();
+            }
+        }
+        
+    }
+
+    private void stopAddingToStack()
+    {
+        StartCoroutine(WaitStackAdd(1));
+        stopper = true;
     }
 }
